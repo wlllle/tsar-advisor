34ba7bd0469e8c0be7950135adf004ec0daad6bf
Viacheslav Kozyrev <weautify@gmail.com>
2023-01-15 13:09:12 +0300

    Open project file on click

--- src/tree/projects/ProjectFileTreeItem.ts
+++ src/tree/projects/ProjectFileTreeItem.ts
@@ -1,12 +1,20 @@
 import * as path from 'path';
-import { TreeItem, TreeItemCollapsibleState } from 'vscode'
+import { TreeItem, TreeItemCollapsibleState, Uri } from 'vscode'
 
 export class ProjectFileTreeItem extends TreeItem {
-  private projectFile: ProjectFileJsonObject;
+  private _projectFile: ProjectFileJsonObject;
+  private _fileName: string;
+  private _fullPath: string;
 
   constructor(projectFile: ProjectFileJsonObject) {
-    let fileName = path.parse(projectFile.file).base
+    let fileName = path.parse(projectFile.file).base;
+    let fullPath = path.join(projectFile.directory, projectFile.file);
     super(fileName, TreeItemCollapsibleState.None);
-    this.projectFile = projectFile;
+    this._projectFile = projectFile;
+    this._fileName = fileName;
+    this._fullPath = fullPath;
+    this.command = {
+      command: "vscode.open", title: "", arguments: [Uri.file(fullPath)]
+    };
   }
 }
